d856e2f58ac74e978adeb599679335f8c957b21c
Spawlak93 <63370588+Spawlak93@users.noreply.github.com>
2022-04-04 10:22:26 -0400

    Initial

---
+++ Pokemon_Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Pokemon_Console;
+
+class Program
+    {
+        static void Main()
+        {
+            ProgramUI program = ProgramUI();
+            program.Run);
+        }
+    }
